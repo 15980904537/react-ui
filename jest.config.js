a210a52b7ba635d99e4f0599d88a5af7509f7400
vagabond370 <8226551+vagabond370@user.noreply.gitee.com>
2021-05-23 10:38:26 +0800

    配置jest单元测试

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    verbose: true,
+    clearMocks: false,
+    collectCoverage: false,
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.test.json',
+        },
+    },
+    reporters: ["default"],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules'],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+        
+       
+    },
+    testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+    transform: {
+        "^.+unit\\.(js|jsx)$": "babel-jest",
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+}
---
+++ lib/__tests__/hello.unit.tsx
@@ -0,0 +1,8 @@
+// test("hello", () => {
+//   expect(1).toEqual(2);
+// });
+describe("我的第一个测试用例", () => {
+  it("1等于1", () => {
+    expect(1).toEqual(1);
+  });
+});
