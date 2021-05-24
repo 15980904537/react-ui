861697bf64e0c34e9601f5184e1e6ca7016e5964
vagabond370 <8226551+vagabond370@user.noreply.gitee.com>
2021-05-24 17:21:02 +0800

    创建icon组件--点击事件的实现(ts)

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./icon.scss";
 import "./importAll";
+// interface IconProps {
+//   name: String;
+//   onClick: (e: React.MouseEvent) => void;
+// }
 interface IconProps {
   name: String;
+  onClick: React.MouseEventHandler<SVGElement>;
 }
 export const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
     <span>
-      <svg className="my-icon">
+      <svg className="my-icon" onClick={props.onClick}>
         <use xlinkHref={`#${props.name}`}></use>
       </svg>
     </span>
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,4 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Icon } from "./Icon";
 
-ReactDOM.render(<Icon name={"wechat"} />, document.querySelector("#root"));
+// function fn(e: React.MouseEvent<SVGAElement | SVGUseElement>) {
+//   //   console.log("1");
+//   console.log(e);
+//   console.log(e.target);
+//   //断言
+//   console.log((e.target as SVGUseElement).href);
+// }
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e);
+};
+ReactDOM.render(
+  <Icon name={"wechat"} onClick={fn} />,
+  document.querySelector("#root")
+);
