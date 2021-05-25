e073aca1cfa0325c81b346336eb956398f4db9fd
vagabond370 <8226551+vagabond370@user.noreply.gitee.com>
2021-05-25 16:47:24 +0800

    测试icon组件--测试onClick点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,12 +1,23 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
+import { mount, shallow } from "enzyme"
 import {
     Icon
 } from '../Icon'
 describe("icon测试", () => {
             it('render successify', () => {
-                    const json = renderer.create( < Icon name = { 'wechat' }
-                        / > ).toJSON()
-                        expect(json).toMatchSnapshot()
+                const json = renderer.create( < Icon name = { 'wechat' }
+                    />
+                ).toJSON();
+                expect(json).toMatchSnapshot()
+            });
+            it('onClick event', () => {
+                    const fn = jest.fn();
+                    console.log(mount)
+                    const component = mount( < Icon name = "wechat"
+                        onClick = { fn }
+                        />);
+                        component.find('svg').simulate('click'); expect(fn).toBeCalled();
+
                     })
             });
