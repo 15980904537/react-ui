f31bcb70a4d3d17c6c6adaa55551f5dd093eaa32
vagabond370 <8226551+vagabond370@user.noreply.gitee.com>
2021-05-26 09:09:57 +0800

    持续集成--ci 添加test-results/jest/result.xml(必须手动自己添加)

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     reporters: ["default", "jest-junit"],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    moduleDirectories: ['node_modules'],
+    moduleDirectories: ['node_modules', 'include'],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
         
