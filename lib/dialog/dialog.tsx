c5ef26d801118b17c831c270636d149a6bfda361
vagabond370 <8226551+vagabond370@user.noreply.gitee.com>
2021-05-28 10:37:13 +0800

    创建dialog组件--重写class类

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,18 +1,20 @@
 import React, { Fragment } from "react";
 import { Icon } from "../icon/Icon";
 import "./dialog.scss";
+import { createClass } from "../utils/classes";
 interface DialogProps {
   visible: boolean;
 }
 export const Dialog: React.FunctionComponent<DialogProps> = (props) => {
+  const sc = createClass("my-dialog");
   return props.visible ? (
     <Fragment>
-      <div className="my-dialog-mask"></div>
-      <div className="my-dialog">
+      <div className={sc("mask")}></div>
+      <div className={sc()}>
         <Icon name="close"></Icon>
-        <header className="my-dialog-header">提示</header>
-        <main className="my-dialog-main">{props.children}</main>
-        <footer className="my-dialog-footer">
+        <header className={sc("header")}>提示</header>
+        <main className={sc("main")}>{props.children}</main>
+        <footer className={sc("footer")}>
           <button>ok</button>
           <button>cancel</button>
         </footer>
--- lib/utils/classes.ts
+++ lib/utils/classes.ts
@@ -1,3 +1,9 @@
 export const classes = (...res: (string | undefined)[]) => {
   return res.filter(Boolean).join(" ");
 };
+
+export const createClass = (prefix: string) => {
+  return function (name?: string) {
+    return [prefix, name].filter(Boolean).join("-");
+  };
+};
